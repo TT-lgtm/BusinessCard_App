874b87c6790121fe520b7d7b2bf5aa42131f9c2e
TT-lgtm <comkosmos.sch@gmail.com>
2026-02-14 14:32:15 +0300

    InProccess(addedtext)

--- app/src/main/java/com/example/firstappandroidstudio/lapteva/reimer/businesscardapp/MainActivity.kt
+++ app/src/main/java/com/example/firstappandroidstudio/lapteva/reimer/businesscardapp/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.firstappandroidstudio.lapteva.reimer.businesscardapp.ui.theme.BusinessCardAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +30,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BusinessCardAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+                    FullName(
+                        name = "Full Name",
+                        titletext="Title",
+                        modifier = Modifier.padding(8.dp)
                     )
                 }
             }
@@ -31,17 +46,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AvatarIcon(message: String, from: String, modifier: Modifier= Modifier)
+{
+    val image = painterResource(R.drawable.icon_avatar)
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.7F
+        )
+    }
 }
+
+@Composable
+fun FullName(name: String, modifier: Modifier = Modifier, titletext: String) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        Text(
+            text = name,
+            modifier = modifier,
+            fontSize = 50.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = titletext,
+            fontSize = 30.sp,
+            textAlign = TextAlign.Center,
+            modifier = modifier
+                .padding(16.dp)
+                //.align(alignment = Alignment.End)
+        )
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BusinessCardAppTheme {
-        Greeting("Android")
+        FullName("Full Name",titletext="Title")
     }
 }
