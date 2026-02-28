344e4f2a3e0ecccc22d1b581c87ff82e80476d4a
TT-lgtm <comkosmos.sch@gmail.com>
2026-02-28 13:24:51 +0300

    Ready_business_card

--- app/src/main/java/com/example/firstappandroidstudio/lapteva/reimer/businesscardapp/MainActivity.kt
+++ app/src/main/java/com/example/firstappandroidstudio/lapteva/reimer/businesscardapp/MainActivity.kt
@@ -6,39 +6,36 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.firstappandroidstudio.lapteva.reimer.businesscardapp.ui.theme.BusinessCardAppTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BusinessCardAppTheme {
-                Surface (
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ){
-                    FullName(
-                        name = "Full Name",
-                        titletext="Title",
-                        modifier = Modifier.padding(8.dp)
-                    )
+            BusinessCardAppTheme{
+                Surface (modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background)
+                {
+                    BusinessCard()
                 }
             }
         }
@@ -46,49 +43,92 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun AvatarIcon(message: String, from: String, modifier: Modifier= Modifier)
-{
-    val image = painterResource(R.drawable.icon_avatar)
-    Box(modifier) {
-        Image(
-            painter = image,
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            alpha = 0.7F
-        )
-    }
-}
-
-@Composable
-fun FullName(name: String, modifier: Modifier = Modifier, titletext: String) {
+fun BusinessCard(){
     Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier
+        modifier = Modifier
             .fillMaxSize()
-            .padding(8.dp)
-    ) {
-        Text(
-            text = name,
-            modifier = modifier,
-            fontSize = 50.sp,
-            textAlign = TextAlign.Center
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Spacer(modifier = Modifier.padding(top = 200.dp))
+        Image(
+            painter = painterResource(id = R.drawable.icon_avatar),
+            contentDescription = "Icon Avatar",
+            modifier = Modifier
+                .size(150.dp)
+                .padding(bottom = 16.dp)
         )
         Text(
-            text = titletext,
-            fontSize = 30.sp,
+            text = "Олег и Валерия",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF018786),
             textAlign = TextAlign.Center,
-            modifier = modifier
-                .padding(16.dp)
-                //.align(alignment = Alignment.End)
+            modifier = Modifier.padding(bottom = 8.dp)
         )
+        Text(
+            text = "Разработчики мобильных приложений",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Normal,
+            color = Color.Blue,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(bottom = 8.dp)
+        )
+        Spacer(modifier = Modifier.padding(top = 300.dp))
+        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(bottom = 8.dp)){
+            Image(
+                painter = painterResource(id = R.drawable.icon_telephone),
+                contentDescription = "Phone Icon",
+                modifier = Modifier
+                    .size(24.dp)
+                    .padding(end = 8.dp)
+            )
+            Text(
+                text = "+7 (987) 654-32-10",
+                fontSize = 16.sp,
+                color = Color.Black,
+                textAlign = TextAlign.Center
+            )
+        }
+        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(bottom = 8.dp)){
+            Image(
+                painter = painterResource(id = R.drawable.icon_telegram),
+                contentDescription = "Telegram Icon",
+                modifier = Modifier
+                    .size(24.dp)
+                    .padding(end = 8.dp)
+            )
+            Text(
+                text = "@telegram",
+                fontSize = 16.sp,
+                color = Color.Black,
+                textAlign = TextAlign.Center
+            )
+        }
+        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(bottom = 8.dp)){
+            Image(
+                painter = painterResource(id = R.drawable.icon_mail),
+                contentDescription = "Email Icon",
+                modifier = Modifier
+                    .size(24.dp)
+                    .padding(end = 8.dp)
+            )
+            Text(
+                text = "email@gmail.com",
+                fontSize = 16.sp,
+                color = Color.Black,
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BusinessCardPreview(){
     BusinessCardAppTheme {
-        FullName("Full Name",titletext="Title")
+        BusinessCard()
     }
 }
