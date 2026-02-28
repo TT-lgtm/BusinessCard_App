8702098180c05c16444b54357c5c27f6d9800eba
TT-lgtm <comkosmos.sch@gmail.com>
2026-02-28 14:01:35 +0300

    Ready_business_card

--- app/src/main/java/com/example/firstappandroidstudio/lapteva/reimer/businesscardapp/MainActivity.kt
+++ app/src/main/java/com/example/firstappandroidstudio/lapteva/reimer/businesscardapp/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -50,25 +51,26 @@ fun BusinessCard(){
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
-    ){
+    ) {
         Spacer(modifier = Modifier.padding(top = 200.dp))
         Image(
             painter = painterResource(id = R.drawable.icon_avatar),
-            contentDescription = "Icon Avatar",
+            contentDescription = stringResource(R.string.icon_avatar),
             modifier = Modifier
                 .size(150.dp)
                 .padding(bottom = 16.dp)
         )
         Text(
-            text = "Олег и Валерия",
+            text = stringResource(R.string.full_name),
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold,
             color = Color(0xFF018786),
             textAlign = TextAlign.Center,
-            modifier = Modifier.padding(bottom = 8.dp)
+            modifier = Modifier
+                .padding(bottom = 8.dp)
         )
         Text(
-            text = "Разработчики мобильных приложений",
+            text = stringResource(R.string.title_text),
             fontSize = 18.sp,
             fontWeight = FontWeight.Normal,
             color = Color.Blue,
@@ -77,46 +79,54 @@ fun BusinessCard(){
                 .padding(bottom = 8.dp)
         )
         Spacer(modifier = Modifier.padding(top = 300.dp))
-        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(bottom = 8.dp)){
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .padding(start = 100.dp),
+        verticalArrangement = Arrangement.Bottom
+    ){
+        Row(modifier = Modifier.padding(bottom = 8.dp)){
             Image(
                 painter = painterResource(id = R.drawable.icon_telephone),
-                contentDescription = "Phone Icon",
+                contentDescription = stringResource(R.string.phone_icon),
                 modifier = Modifier
                     .size(24.dp)
                     .padding(end = 8.dp)
             )
             Text(
-                text = "+7 (987) 654-32-10",
+                text = stringResource(R.string.phone_number),
                 fontSize = 16.sp,
                 color = Color.Black,
                 textAlign = TextAlign.Center
             )
         }
-        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(bottom = 8.dp)){
+        Row(modifier = Modifier.padding(bottom = 8.dp)){
             Image(
                 painter = painterResource(id = R.drawable.icon_telegram),
-                contentDescription = "Telegram Icon",
+                contentDescription = stringResource(R.string.telegram_icon),
                 modifier = Modifier
                     .size(24.dp)
                     .padding(end = 8.dp)
             )
             Text(
-                text = "@telegram",
+                text = stringResource(R.string.name_telegram),
                 fontSize = 16.sp,
                 color = Color.Black,
                 textAlign = TextAlign.Center
             )
         }
-        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(bottom = 8.dp)){
+        Row(modifier = Modifier.padding(bottom = 8.dp)) {
             Image(
                 painter = painterResource(id = R.drawable.icon_mail),
-                contentDescription = "Email Icon",
+                contentDescription = stringResource(R.string.email_icon),
                 modifier = Modifier
                     .size(24.dp)
                     .padding(end = 8.dp)
             )
             Text(
-                text = "email@gmail.com",
+                text = stringResource(R.string.email),
                 fontSize = 16.sp,
                 color = Color.Black,
                 textAlign = TextAlign.Center
